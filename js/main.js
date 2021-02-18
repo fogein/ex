9d45838ca95daf7b276663a7ad37b6d5b3bd1c35
fogein <77330295+fogein@users.noreply.github.com>
2021-02-19 01:16:34 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slide-button-next',
+    prevEl: '.slide-button-prev',
+  },
+});
